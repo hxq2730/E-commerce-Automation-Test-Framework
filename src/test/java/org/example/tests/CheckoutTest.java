bf18febba02ac21f0f57c31e3a6fe4f56f536ce3
hxq2730 <hoxuanquangqt@gmal.com>
2026-01-26 18:43:38 +0700

    Remove unused tests

--- src/test/java/org/example/tests/CheckoutTest.java
+++ src/test/java/org/example/tests/CheckoutTest.java
@@ -89,59 +89,59 @@ public class CheckoutTest extends BaseTest {
         LogUtils.info("✅ Order verified successfully in History!");
     }
 
-    @Test(description = "E2E: Guest User -> Cart -> Force Login -> Checkout -> Order Success")
-    @Severity(SeverityLevel.NORMAL)
-    public void testGuestCheckoutWithLoginRedirect() {
-        // 1. Init Guest User
-        homePage = new HomePage();
-        homePage.openHomePage();
-
-        // 2. Search & Add to Car
-        searchPage = homePage.searchForProduct("dell");
-        productDetailsPage = searchPage.clickFirstProduct();
-        productDetailsPage.addToCart();
-
-        // Assert Add Success
-        Assert.assertNotNull(productDetailsPage.getAddToCartSuccessMessage(),
-                "FAIL: Product not added to cart.");
-
-        // 3. Go to Cart Page
-        productDetailsPage.clickCloseModel();
-        cartPage = productDetailsPage.goToCartPage();
-
-        // 4. Proceed to Checkout
-        cartPage.clickContinueToShipping();
-
-        // 5. Handle Login Modal
-        if (cartPage.isLoginModalDisplay()) {
-            cartPage.loginInModal(FrameworkConstants.DEFAULT_EMAIL, FrameworkConstants.DEFAULT_PASSWORD);
-            LogUtils.info("Performing 2nd Click on Checkout button after Login...");
-            cartPage.clickContinueToShipping();
-        } else {
-            LogUtils.info("Login Modal did not appear (User might be already logged in).");
-        }
-
-        // 6. Init Checkout Page
-        checkoutPage = new CheckoutPage();
-        checkoutPage.checkoutSuccessfully("123 Street", "Vietnam", "Dong Thap", "Cao Lanh", "1000"
-                , "0987654321", "fragile goods");
-
-        // 7. Verify Success
-        String confirmMsg = checkoutPage.getOrderConfirmationMessage();
-        Assert.assertNotNull(confirmMsg, "FAIL: Order confirmation message is null (Order likely " +
-                "failed).");
-
-        Assert.assertTrue(confirmMsg.toLowerCase().contains("thank you"), "FAIL: Success message " +
-                "content is incorrect. Found: " + confirmMsg);
-
-        // 8. Verify order in Order History
-        String orderCode = checkoutPage.getOrderCode();
-        LogUtils.info("Order code: " + orderCode);
-
-        orderHistoryPage = new OrderHistoryPage();
-        String firstOrderCode = orderHistoryPage.getLatestOrderCode();
-
-        Assert.assertEquals(orderCode, firstOrderCode, "❌ Order Code does not match!");
-        LogUtils.info("✅ Order verified successfully in History!");
-    }
+//    @Test(description = "E2E: Guest User -> Cart -> Force Login -> Checkout -> Order Success")
+//    @Severity(SeverityLevel.NORMAL)
+//    public void testGuestCheckoutWithLoginRedirect() {
+//        // 1. Init Guest User
+//        homePage = new HomePage();
+//        homePage.openHomePage();
+//
+//        // 2. Search & Add to Car
+//        searchPage = homePage.searchForProduct("dell");
+//        productDetailsPage = searchPage.clickFirstProduct();
+//        productDetailsPage.addToCart();
+//
+//        // Assert Add Success
+//        Assert.assertNotNull(productDetailsPage.getAddToCartSuccessMessage(),
+//                "FAIL: Product not added to cart.");
+//
+//        // 3. Go to Cart Page
+//        productDetailsPage.clickCloseModel();
+//        cartPage = productDetailsPage.goToCartPage();
+//
+//        // 4. Proceed to Checkout
+//        cartPage.clickContinueToShipping();
+//
+//        // 5. Handle Login Modal
+//        if (cartPage.isLoginModalDisplay()) {
+//            cartPage.loginInModal(FrameworkConstants.DEFAULT_EMAIL, FrameworkConstants.DEFAULT_PASSWORD);
+//            LogUtils.info("Performing 2nd Click on Checkout button after Login...");
+//            cartPage.clickContinueToShipping();
+//        } else {
+//            LogUtils.info("Login Modal did not appear (User might be already logged in).");
+//        }
+//
+//        // 6. Init Checkout Page
+//        checkoutPage = new CheckoutPage();
+//        checkoutPage.checkoutSuccessfully("123 Street", "Vietnam", "Dong Thap", "Cao Lanh", "1000"
+//                , "0987654321", "fragile goods");
+//
+//        // 7. Verify Success
+//        String confirmMsg = checkoutPage.getOrderConfirmationMessage();
+//        Assert.assertNotNull(confirmMsg, "FAIL: Order confirmation message is null (Order likely " +
+//                "failed).");
+//
+//        Assert.assertTrue(confirmMsg.toLowerCase().contains("thank you"), "FAIL: Success message " +
+//                "content is incorrect. Found: " + confirmMsg);
+//
+//        // 8. Verify order in Order History
+//        String orderCode = checkoutPage.getOrderCode();
+//        LogUtils.info("Order code: " + orderCode);
+//
+//        orderHistoryPage = new OrderHistoryPage();
+//        String firstOrderCode = orderHistoryPage.getLatestOrderCode();
+//
+//        Assert.assertEquals(orderCode, firstOrderCode, "❌ Order Code does not match!");
+//        LogUtils.info("✅ Order verified successfully in History!");
+//    }
 }
